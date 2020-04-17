c2575345b8d43713e9d90cb9011211ce1ef4bfdd
Chetna Aggarwal <chetnaaggarwal@Chetnas-MacBook-Pro.local>
2020-04-17 22:22:09 +0530

    correctedssl

--- server/config/config.js
+++ server/config/config.js
@@ -26,10 +26,10 @@ module.exports = {
         database: process.env.MYSQL_DATABASE,
         host: process.env.MYSQL_HOST,
         port: process.env.MYSQL_PORT,
-        ssl: true,
+        ssl: false,
         dialectOptions: {
             ssl: {
-                encrypt: true,
+                encrypt: false,
                 ca: fs.readFileSync(path.resolve(__dirname, "./BaltimoreCyberTrustRoot.crt.pem"))
             }
         }
