60a8327bf4d3f7d1559806ed45ec8fb78f085b02
Chetna Aggarwal <chetnaaggarwal@Chetnas-MacBook-Pro.local>
2020-04-18 12:47:06 +0530

    updated variable

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverRoot: "https://nodejsudemy.azurewebsites.net/api",
+  serverRoot: "https://nodemigrationlinux.azurewebsites.net/api",
   endpoints: {
     auth: {
       signUp: "/users/signup",
